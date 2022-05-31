35aaca62e7f2ba423fca70eef343ac3fcdf95260
Mathieu Chartier <mathieuc@google.com>
2022-06-01 00:42:41 +0100

    fog: Always preopt extracted APKs
    Signed-off-by: shield44 <shield44@pixelexperience.org>
    Change-Id: Id609880a86ed505106debf83dc7af4edeaa358df

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@ $(call inherit-product, vendor/xiaomi/fog/fog-vendor.mk)
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
